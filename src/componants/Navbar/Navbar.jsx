0879e1c6c2177c08b22fc74e0868bd8c665d7539
Priyanshu <56836660+priyanshuvatsyan@users.noreply.github.com>
2024-10-26 18:54:54 +0530

    ready for deployment

--- src/componants/Navbar/Navbar.jsx
+++ src/componants/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
-
 import './Navbar.css';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Toggle menu on small screens
+  };
+
   return (
     <nav className="navbar">
       <h1 className="logo">{"<Pri.Va/>"}</h1>
-      <ul>
+      
+      <div className="menu-icon" onClick={toggleMenu}>
+        <span className="navbar-toggler-icon">☰</span>
+      </div>
+
+      <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
         <li><a href="/">Home</a></li>
         <li><a href="/project">Projects</a></li>
         <li><a href="/about">About</a></li>
